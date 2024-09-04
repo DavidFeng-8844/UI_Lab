7fb67abec5ae9381bf5a6fbf6e3c824a791a5224
DavidFeng-8844 <davidfeng.8844@gmail.com>
2024-09-04 10:59:46 +0800

    update lab1 ex1

---
+++ Lab1_exercise1/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main() {
+	double x, y;
+	cout << "Enter x & y coordinates: ";
+    cin >> x >> y;
+	cout << "Coordinates are (" << x << "," << y <<")" << endl;
+}
+
+
+
+
+
+
+
+
