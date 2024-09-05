99e8fd1d051568533b508ed433290d6f3eb1450e
DavidFeng-8844 <davidfeng.8844@gmail.com>
2024-09-05 11:49:29 +0800

    update lab1 ex6 with range-based for loop to assign value to the objects in vectors

--- Lab1_exercise6/main.cpp
+++ Lab1_exercise6/main.cpp
@@ -9,6 +9,8 @@ class Person {
         string name;
         int age;
 
+    Person () : name("unknown") {} //Default constructor with no parameters
+
     Person(string n, int a) : name(n), age(a) {
     }
 
@@ -19,17 +21,27 @@ class Person {
 };
 int main()
 {
-    vector<Person> dudes;
     cout << "Insert names and ages" << endl;
-    for(/*auto myDude : dudes*/int i = 0; i < 3; i++) {
+    ///// Add elements dynamically with emplace_back() /////
+    // vector<Person> dudes;
+    // for(/*auto myDude : dudes*/int i = 0; i < 3; i++) {
+    //     string tempName;
+    //     int tempAge;
+    //     cin >> tempName >> tempAge;
+    //     // myDude(tempName, tempAge);
+    //     dudes.emplace_back(tempName, tempAge);
+    // }
+
+    ///// Use range-based for loop to assign values to existing objects /////
+    vector<Person> dudes(3);
+    for (auto& myDudes : dudes) {
         string tempName;
         int tempAge;
         cin >> tempName >> tempAge;
-        // myDude(tempName, tempAge);
-        dudes.emplace_back(tempName, tempAge);
-
+        myDudes.name = tempName;
+        myDudes.age= tempAge;
     }
-    for(auto guy : dudes) {
+    for(auto& guy : dudes) {
         guy.printName();
     }
 
